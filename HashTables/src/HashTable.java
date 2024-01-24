ae25e85619328d1ef6dea9141758bf057d79a189
John Karmozyn <hughkarmozyn@icloud.com>
2024-01-24 11:28:32 -0700

    bare methods

---
+++ HashTables/src/HashTable.java
@@ -0,0 +1,27 @@
+public class HashTable {
+
+    int[] hashCode;
+    int size;
+    String[] values;
+    public HashTable(int capacity){
+        hashCode = new int[capacity];
+        values = new String[capacity];
+
+    }
+
+
+    public boolean put(String key, String value){
+
+    }
+
+    //get hashes the key to get the index, and returns that element. Returns null if key not found.
+    public String get(String key){
+
+
+    }
+
+    //returns the unique int in the range of the [0, array length)
+    private int hashCode(String key){
+
+    }
+}
